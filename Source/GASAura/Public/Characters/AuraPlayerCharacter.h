d9b98990271e6e1dd20822014084db0a69b4a319
v_hlhllu <v_hlhllu@tencent.com>
2024-05-18 16:30:26 +0800

    ==== P3~6 ====  1. [C++] 人物基类(附加插槽)，玩家，敌人  2. [UE] 人物蓝图（添加骨骼体和武器）  3. [UE] 动画蓝图、动画模板（状态机，初始化）

---
+++ Source/GASAura/Public/Characters/AuraPlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/AuraCharacterBase.h"
+#include "AuraPlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GASAURA_API AAuraPlayerCharacter : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
