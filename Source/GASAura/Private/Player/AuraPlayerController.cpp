f88db97eb17314c6289399bd93b460647298542a
v_hlhllu <v_hlhllu@tencent.com>
2024-05-20 17:11:36 +0800

    ==== P7~9 ==== 1. [UE] 创建输入行为和输入映射蓝图 2. [C++] 绑定增强输入和控制器、实现移动函数

---
+++ Source/GASAura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,69 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/AuraPlayerController.h"
+#include "Player/AuraPlayerController.h"
+#include "Player/AuraPlayerController.h"
+
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(AuraContext);
+
+	/*绑定输入映射到Controller
+	 ************************************ begin ************************************/
+	
+	// 添加输入映射到增强输入本地播放子系统
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	// 设置鼠标样式和显隐
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	// 设置输入模式
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+	
+	/************************************ end ************************************/
+}
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	// tips：当控制器拥有输入组件后，就会绑定移动输入
+	// 将输入组件转换为增强输入组件，并进行检查
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move);
+}
+
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	if (APawn* ControlledPawn = GetPawn<APawn>())
+	{
+		// 取控制器旋转体的Yaw值
+		const FRotator Rotation = GetControlRotation();
+		const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+		// 旋转矩阵（将旋转体转为矩阵，即2d矢量），并将对应轴的单位值赋给方向vector
+		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+		// 取平面方向的输入值（0|1），用于控制移动方向
+		const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+		// 因世界为右手坐标系，轴输入为左手坐标系，因此XY方向不同
+		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
+}
